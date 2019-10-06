d351cde2f47c4bfb74a174ce6231355b09704187
Anton Mayatskiy <worknekki@gmail.com>
2019-10-06 11:53:51 +1000

    Linear chart created

--- src/assets/js/modules/Charts/LinearChart.js
+++ src/assets/js/modules/Charts/LinearChart.js
@@ -17,7 +17,7 @@ d3.timeFormatDefaultLocale({
 
 function draw(data) {
   const margin = { top: 20, right: 20, bottom: 50, left: 50 };
-  const width = 920 - margin.left - margin.right;
+  const width = 420 - margin.left - margin.right;
   const height = 390 - margin.top - margin.bottom;
 
   const x = d3.scaleTime()
